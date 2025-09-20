b8bce4365747bcce728c33aeb7c1a68bd05eeb4d
gridout@gmail.com <gridout@gmail.com>
2025-09-20 02:39:56 +0100

    Major cleanup: Remove and recreate project files and dependencies

--- VectorCode/lua/vectorcode/integrations/codecompanion/prompts/presets.lua
+++
@@ -1,9 +0,0 @@
----@type table<string, VectorCode.CodeCompanion.PromptFactory.Opts>
-local M = {}
-
-M["Neovim Tutor"] = {
-  project_root = vim.fs.normalize(vim.env.VIMRUNTIME),
-  file_patterns = { "lua/**/*.lua", "doc/**/*.txt" },
-}
-
-return M
